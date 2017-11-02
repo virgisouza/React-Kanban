5d2444e3246c571ad936986019364f77b1e8c45e
Virginia Souza <virginiasouza@Virginias-MacBook-Pro.local>
2017-11-02 09:23:43 -1000

    actions function to add new cards

--- src/actions/cards.js
+++ src/actions/cards.js
@@ -3,6 +3,13 @@ export const LOAD_CARDS = 'LOAD_CARDS';
 export const loadCards = (cards) => {
 
   return function (dispatch){
+
+    //GET method
+    // var oReq = new XMLHttpRequest();
+    // oReq.addEventListener("load", reqListener);
+    // oReq.open("GET", "http://www.example.org/example.txt");
+    // oReq.send();
+
     //add function here to loadcard data
   // return {
   //   type: LOAD_CARDS,
@@ -12,4 +19,6 @@ export const loadCards = (cards) => {
 
 export const addNewCard =  (newCard) => {
   //add function to add new card to db
+
+  //POST method
 }
